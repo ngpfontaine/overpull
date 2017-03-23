670cc2b394162c173c61ac9714a7cdb4088156d9
Nic Fontaine <ngpfontaine@gmail.com>
2017-03-22 17:20:15 -0700

    mobile & desktop

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ var msg = document.getElementById('msg');
 var height = pull.clientHeight;
 var cursorClickOffset = 0;
 wHeight = window.outerHeight;
+
 var maxH = 120;
 pull.style.maxHeight = maxH + 'px';
 
@@ -12,13 +13,13 @@ var touchEvDown = 'ontouchstart' in window ? 'touchstart' : 'mousedown';
 var touchEvUp = 'ontouchend' in window ? 'touchend' : 'mouseup';
 var touchEvMove = 'ontouchmove' in window ? 'touchmove' : 'mousemove';
 
-// var yPos = 'ontouchstart' in window ? targetTouches[0].pageY : clientY;
+var mobile = 'ontouchstart' in window ? true : false;
 
 window.onscroll = function(ev) {
   if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {       
     document.addEventListener(touchEvDown, function foo(e) {
       pullTouchOn(e);
-      cursorClickOffset = wHeight-(e.targetTouches[0].pageY);
+      cursorClickOffset = mobile ? wHeight-(e.targetTouches[0].pageY) : wHeight-(e.clientY);
     });
     document.addEventListener(touchEvUp, pullTouchOff);
   }
@@ -44,8 +45,8 @@ function pullTouchOff() {
 
 function pullHeight(inp,trueFalse) {
   if (trueFalse) {
-    var pullHeightZeroed = (window.outerHeight-inp.targetTouches[0].pageY)-cursorClickOffset;
-    overpull.style.height = pullHeightZeroed/2 + 'px';
+    var pullHeightZeroed = mobile ? (window.outerHeight-inp.targetTouches[0].pageY)-cursorClickOffset : (window.outerHeight-inp.clientY)-cursorClickOffset;
+    overpull.style.height = (pullHeightZeroed/2) + 'px';
     if ((pullHeightZeroed*2/3) > (maxH-20)) {
       // pull.style.maxHeight = (maxH+10) + 'px';
       pull.style.minHeight = '40px';
